f5fcc8a6fcfdfdcf367ad631c8ec992b7508d3ce
Kamilos3312 <Kamilos3312@gmail.com>
2018-05-07 18:44:48 +0200

    Added support for gen. algorithm settings

--- src/main/code/com/pz/JavaFX/Controllers/KnapsackController.java
+++ src/main/code/com/pz/JavaFX/Controllers/KnapsackController.java
@@ -3,8 +3,11 @@ package com.pz.JavaFX.Controllers;
 import com.pz.Models.Item;
 import com.pz.Models.ItemsPool;
 import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -21,7 +24,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class KnapsackController implements Initializable {
-    //  FXML Interface
+//  FXML Interface
 //  Buttons
     @FXML private Button addItemButton, removeItemButton;
     @FXML private Button calculateButton, resetButton, returnButton;
@@ -29,7 +32,7 @@ public class KnapsackController implements Initializable {
     @FXML private TextField populationSizeField;
     @FXML private Slider mutationSlider, targetIdenticalSlider;
     @FXML private TextField knapsackCapacityField;
-    @FXML private Label mutationChanceLabel, targetIdenticalLabel;
+    @FXML private Label mutationChanceLabel, TargetIdenticalLabel;
 
     //  Selected items summary
     @FXML private Label totalCount, totalValue, totalWeight;
@@ -45,6 +48,9 @@ public class KnapsackController implements Initializable {
     private ObservableList<Item> selectedItems;
     private ItemsPool selectedItemsPool = new ItemsPool();
 
+    private Integer populationSize, mutationChance, targetIdentical;
+    private Double knapsackSize;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         items = ItemManagementController.itemList;
@@ -58,9 +64,38 @@ public class KnapsackController implements Initializable {
         sweight.setCellValueFactory(new PropertyValueFactory<Item, Double>("weight"));
         itemTable.setItems(items);
 
-//        mutationSlider.setValue(0);
-//        mutationChanceLabel.textProperty().bind(new SimpleDoubleProperty(mutationSlider.getValue()).asString());
-//        targetIdenticalLabel.textProperty().bind(new SimpleDoubleProperty(targetIdenticalSlider.getValue()).asString());
+        mutationSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                                Number old_val, Number new_val) {
+                mutationChanceLabel.setText(String.format("%d", new_val.intValue()) + "%");
+                mutationChance = new_val.intValue();
+                System.out.println("Mutation chance: " + mutationChance);
+            }
+        });
+        targetIdenticalSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                                Number old_val, Number new_val) {
+                TargetIdenticalLabel.setText(String.format("%d", new_val.intValue()) + "%");
+                targetIdentical = new_val.intValue();
+                System.out.println("Target Identical %: " + targetIdentical);
+            }
+        });
+        populationSizeField.textProperty().addListener(new ChangeListener<String>() {
+            public void changed(ObservableValue<? extends String> ov,
+                                String old_val, String new_val) {
+                if (new_val.isEmpty())  return;
+                populationSize = Integer.valueOf(new_val);
+                System.out.println("Population size: " + populationSize);
+            }
+        });
+        knapsackCapacityField.textProperty().addListener(new ChangeListener<String>() {
+            public void changed(ObservableValue<? extends String> ov,
+                                String old_val, String new_val) {
+                if (new_val.isEmpty())  return;
+                knapsackSize = Double.valueOf(new_val);
+                System.out.println("Knapsack size: " + knapsackSize);
+            }
+        });
     }
 
     @FXML protected void addItem() {
@@ -111,10 +146,6 @@ public class KnapsackController implements Initializable {
         }catch(IOException e) {
             e.printStackTrace();
         }
-    }
-
-    @FXML protected void updateMutationLabel() {
-        mutationChanceLabel.setText(Double.toString(mutationSlider.getValue()));
     }
 
     @FXML protected void returnToMainMenu() throws IOException {
